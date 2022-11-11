cf74f635814880f9a1ec6158db3a70a2813fe170
BaconB0T <brownev19@gcc.edu>
2022-11-11 11:02:47 -0500

    Initial Commit

---
+++ GCCTourGuide/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GCCTourGuide
+//
+//  Created by Ethan Brown on 11/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GCCTourGuide/GCCTourGuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  GCCTourGuideApp.swift
+//  GCCTourGuide
+//
+//  Created by Ethan Brown on 11/11/22.
+//
+
+import SwiftUI
+
+@main
+struct GCCTourGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
