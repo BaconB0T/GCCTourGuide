9b8126e5ad874305d2e0237510c26151ca34ce5f
Tyler Ridout <ridoutTS19@gcc.edu>
2022-11-12 15:09:16 -0500

    Added views and json model

---
+++ Views/Tab1View.swift
@@ -0,0 +1,20 @@
+//
+//  Tab1View.swift
+//  GCCTourGuide
+//
+//  Created by Tyler Ridout on 11/12/22.
+//
+
+import SwiftUI
+
+struct Tab1View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Tab1View_Previews: PreviewProvider {
+    static var previews: some View {
+        Tab1View()
+    }
+}
---
+++ Views/Tab2View.swift
@@ -0,0 +1,20 @@
+//
+//  Tab2View.swift
+//  GCCTourGuide
+//
+//  Created by Tyler Ridout on 11/12/22.
+//
+
+import SwiftUI
+
+struct Tab2View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Tab2View_Previews: PreviewProvider {
+    static var previews: some View {
+        Tab2View()
+    }
+}
