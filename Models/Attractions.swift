0fc4eafef608e07b6d613bb8062fcf7f42f43d45
BaconB0T <brownev19@gcc.edu>
2022-12-02 11:55:25 -0500

    Added long and lat data, and a few more locations as well

--- Models/Attractions.swift
+++ Models/Attractions.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import CoreLocation
 
 // MARK: - AllAttractions
 struct AllAttractions: Codable {
@@ -17,9 +18,16 @@ struct AllAttractions: Codable {
 struct Attraction: Codable, Identifiable {
     var id: UUID?
     let name, shortName, description: String
+    var location: Location
 
     enum CodingKeys: String, CodingKey {
-        case name, description
+        case name, description, location
         case shortName = "short_name"
     }
+}
+
+// MARK: - Location
+struct Location: Codable {
+    var lat: Double
+    var long: Double
 }
